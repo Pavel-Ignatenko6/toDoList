5136137924ed7ef5ef49bf13f4ef5fc9b56538fa
Pavel Ignatenko <pavel.ignatenko1996@gmail.com>
2024-06-14 20:06:06 +0800

    create router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './app.jsx'
+import NotFoundPage from './pages/NotFoundPage.jsx'
+import Layout from './Components/Layout/Layout.jsx'
+import AboutPage from './pages/AboutPage.jsx'
+import ContactsPage from './pages/ContactsPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+      },
+      {
+        path: '/about',
+        element: <AboutPage />,
+      },
+      {
+        path: '/contacts',
+        element: <ContactsPage />,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
