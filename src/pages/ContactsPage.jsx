538733945dc3a8bb4e4f634e782f104594c4dd64
Pavel Ignatenko <pavel.ignatenko1996@gmail.com>
2024-06-18 20:11:54 +0800

    add form validation functionality

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,66 +1,140 @@
+import { useState } from 'react'
+
 const inputMap = [
   {
     id: 1,
     name: 'Full name',
     type: 'text',
     placeholder: 'Enter your Full name',
-    required: true,
+    errorMessage: 'Please enter at least 4 characters',
   },
   {
     id: 2,
     name: 'Age',
     type: 'number',
     placeholder: 'Enter number from 1 to 99',
-    required: true,
-    min: 1,
-    max: 99,
+    errorMessage: 'Please enter a number between 1 and 99',
   },
   {
     id: 3,
     name: 'Email',
     type: 'email',
     placeholder: 'Enter your Email',
-    required: true,
+    errorMessage: 'Please enter a valid email address',
   },
 ]
 
 const radioBtns = ['I love it', `It's allright`, 'Not really', 'Not at all']
 
 export default function ContactsPage() {
+  // set state with erros object
+  const [errors, setErrors] = useState({
+    'Full name': false,
+    Age: false,
+    Email: false,
+  })
+
+  const [inputsValues, setInputsValues] = useState({
+    'Full name': '',
+    Age: '',
+    Email: '',
+  })
+
+  const [radioValue, setRadioValue] = useState('')
+  const [radioChecked, setRadioChecked] = useState(null)
+  const [failedToSubmit, setFailedToSubmit] = useState(null)
+  const [textAreaValue, setTextAreaValue] = useState('')
+  console.log(failedToSubmit)
+  // validation func v
+  const handleValidation = e => {
+    const { name, value } = e.target
+    setErrors({
+      ...errors,
+      [name]: false,
+    })
+    if (value.length < 4 && name !== 'Age') {
+      setErrors({
+        ...errors,
+        [name]: true,
+      })
+    }
+    if ((name === 'Age' && value < 1) || value > 99) {
+      setErrors({
+        ...errors,
+        [name]: true,
+      })
+    }
+    setInputsValues({
+      ...inputsValues,
+      [name]: value,
+    })
+  }
+
+  console.log(inputsValues);
+
+  // submit func
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (errors['Full name'] || errors.Age || errors.Email || radioValue === '' || textAreaValue === '') {
+      setFailedToSubmit(true)
+      return false
+    } else {
+      setFailedToSubmit(false)
+      setRadioValue(null)
+      setTextAreaValue('')
+      setInputsValues({
+        'Full name': '',
+        Age: '',
+        Email: '',
+      })
+    }
+  }
+
   return (
     <div className="contacts-page">
       <h2 className="contacts-text">Please let me know what you like about the app</h2>
-      <form className="form-container">
+      <form className="form-container" onSubmit={handleSubmit}>
         {inputMap.map(input => {
           return (
-            <label key={input.id} className="label-wrapper" htmlFor="input">
-              <div className="label-name">{input.name}</div>
-              <input
-                className="input"
-                type={input.type}
-                name="input"
-                placeholder={input.placeholder}
-                min={input.min}
-                max={input.max}
-                required={input.required}
-              />
-            </label>
+            <div key={input.id} className="single-input">
+              <label className="label-wrapper" htmlFor={input.name}>
+                <div className="label-name">{input.name}</div>
+                <input
+                  className={`input ${errors[input.name] ? 'input-error' : ''}`}
+                  value={inputsValues[input.name]}
+                  onChange={(e) => handleValidation(e)}
+                  type={input.type}
+                  name={input.name}
+                  placeholder={input.placeholder}
+                />
+              </label>
+              <div className="inputs-alert">{errors[input.name] && <p>{input.errorMessage}</p>}</div>
+            </div>
           )
         })}
+        {<div className="error"></div>}
         <div className="form-radio">
           <span className="form-question">Do you enjoy the app?</span>
           {radioBtns.map((radioBtn, index) => {
             return (
               <label key={index} className="radio-wrapper" htmlFor="radioBtn">
-                <input className="radio-btn" type="radio" name="radioBtn" required />
+                <input className="radio-btn" type="radio" name="radioBtn" value={radioBtn} onClick={() => setRadioValue(radioBtn)}  />
                 <div className="radio-name">{radioBtn}</div>
               </label>
             )
           })}
+          {failedToSubmit && radioValue === '' && <div className="radio-alert">Please choose an option</div>}
         </div>
         <div className="review-container">
           <span>Please let me know what I should add or fix</span>
-          <textarea className="review-field" name="review" placeholder="Enter your review here" required></textarea>
+          <textarea
+            className="review-field"
+            name="review"
+            placeholder="Enter your review here"
+            onChange={e => setTextAreaValue(e.target.value)}
+            value={textAreaValue}
+          ></textarea>
+          {failedToSubmit && textAreaValue.length < 30 && <div className="radio-alert">Please type at least 30 characters</div>}
         </div>
         <button className="submit-btn btn" type="submit">
           Submit
